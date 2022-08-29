dad25941369ebe94a90351693359859dc56bba81
Roland Ebrecht <roland.ebrecht@hs-augsburg.com>
2022-08-29 22:50:15 +0200

    Corrected text positiion

--- src/field/place.rs
+++ src/field/place.rs
@@ -1,5 +1,5 @@
+use super::{SCALE};
 use nannou::prelude::*;
-use super::{SCALE, N};
 
 #[derive(Copy, Clone)]
 pub struct Place {
@@ -11,59 +11,69 @@ pub struct Place {
 impl Place {
     pub fn new() -> Self {
         Self {
-            number : 0, 
-            hidden : true,
-            marker : false,
+            number: 0,
+            hidden: true,
+            marker: false,
         }
     }
 
-    pub fn is_bomb(& self)->bool {
+    pub fn is_bomb(&self) -> bool {
         self.number == 10
     }
 
-    pub fn is_empty(& self)->bool 
-    {
+    pub fn is_empty(&self) -> bool {
         self.number == 0
     }
 
-    pub fn is_hidden(& self)->bool {
+    pub fn is_hidden(&self) -> bool {
         self.hidden
     }
 
-    pub fn set_bomb(& mut self) {
+    pub fn set_bomb(&mut self) {
         self.number = 10;
     }
 
-    pub fn uncover(& mut self) {
+    pub fn uncover(&mut self) {
         self.hidden = false;
     }
 
-    pub fn toggle_marker(& mut self) {
+    pub fn toggle_marker(&mut self) {
         self.marker = !self.marker;
     }
 
-    pub fn draw(& self, draw: & Draw, x: usize, y: usize) {
-        let (x_temp ,y_temp) = field_to_cord(x, y);
+    pub fn draw(&self, draw: &Draw, x: usize, y: usize) {
+        let (x_temp, y_temp) = field_to_cord(x, y);
         let size = SCALE * 0.92;
         if self.is_hidden() {
-            draw.rect().x_y(x_temp, y_temp).w_h(size, size).color(DARKGREY);
+            draw.rect()
+                .x_y(x_temp, y_temp)
+                .w_h(size, size)
+                .color(DARKGREY);
             if self.marker {
-                draw.ellipse().x_y(x_temp, y_temp).w_h(size / 2.0, size / 2.0).color(BLUE);
+                draw.ellipse()
+                    .x_y(x_temp, y_temp)
+                    .w_h(size / 2.0, size / 2.0)
+                    .color(BLUE);
             }
         } else {
             if self.is_bomb() {
                 draw.rect().x_y(x_temp, y_temp).w_h(size, size).color(RED);
             } else if self.is_empty() {
-                draw.rect().x_y(x_temp, y_temp).w_h(size, size).color(LIGHTGRAY);                
+                draw.rect()
+                    .x_y(x_temp, y_temp)
+                    .w_h(size, size)
+                    .color(LIGHTGRAY);
             } else {
-                draw.text(&self.number.to_string()).x_y(x_temp, y_temp + 1.0).w_h(SCALE, SCALE).color(BLACK).font_size((SCALE as u32) - 2);
+                draw.text(&self.number.to_string())
+                    .x_y(x_temp, y_temp + 1.0)
+                    .w_h(SCALE, SCALE)
+                    .color(BLACK)
+                    .font_size((SCALE as u32) - 2);
             }
         }
     }
 }
 
-fn field_to_cord(x: usize, y: usize) -> (f32, f32){
-    let x_temp = map_range(x as f32, 0.0, N as f32, -(N as f32 / 2.0) * SCALE, (N as f32 / 2.0) * SCALE) + SCALE / 2.0;
-    let y_temp = map_range(y as f32, 0.0, N as f32, -(N as f32 / 2.0) * SCALE, (N as f32 / 2.0) * SCALE) + SCALE / 2.0;
-    (x_temp, y_temp)
+fn field_to_cord(x: usize, y: usize) -> (f32, f32) {
+    (x as f32 * SCALE, y as f32 * SCALE)
 }
--- src/main.rs
+++ src/main.rs
@@ -4,57 +4,68 @@ use field::*;
 use nannou::prelude::*;
 
 const N: usize = 20;
-const SCALE : f32 = 30.0;
+const SCALE: f32 = 30.0;
 const BOMBS_NUMBER: usize = 50;
 
-
 fn cord_2_field(point: Point2) -> (isize, isize) {
-    let x_temp = map_range(point.x, -(N as f32) * 0.5 * SCALE, (N as f32) * 0.5 * SCALE, 0.0, N as f32);
-    let y_temp = map_range(point.y, -(N as f32) * 0.5 * SCALE, (N as f32) * 0.5 * SCALE, 0.0, N as f32);
+    let x_temp = map_range(
+        point.x,
+        -(N as f32) * 0.5 * SCALE,
+        (N as f32) * 0.5 * SCALE,
+        0.0,
+        N as f32,
+    );
+    let y_temp = map_range(
+        point.y,
+        -(N as f32) * 0.5 * SCALE,
+        (N as f32) * 0.5 * SCALE,
+        0.0,
+        N as f32,
+    );
     (x_temp as isize, y_temp as isize)
 }
-
 
 fn main() {
     nannou::app(model).update(update).run();
 }
 
-struct Model 
-{
+struct Model {
     playground: Field,
     success: bool,
-    bomb: bool
+    bomb: bool,
 }
 
-
 impl Model {
-    fn new() -> Self { 
-        Self { 
-            playground: Field::new(), 
-            success : false, 
-            bomb : false
+    fn new() -> Self {
+        Self {
+            playground: Field::new(),
+            success: false,
+            bomb: false,
         }
     }
 
-    fn draw(&self, draw : &Draw) {
+    fn draw(&self, draw: &Draw) {
         for x in 0..N {
             for y in 0..N {
                 self.playground.field[x][y].draw(draw, x, y);
             }
         }
         if self.success {
-            draw.text("Solved").x_y(0.0, 0.0).color(GREEN).font_size((SCALE as u32) * 2);
+            draw.text("Solved")
+                .x_y((N / 2) as f32 * SCALE, (N / 2) as f32 * SCALE)
+                .color(GREEN)
+                .font_size((SCALE as u32) * 2);
         }
         if self.bomb {
-            draw.text("BAAAM").x_y(0.0, 0.0).color(RED).font_size((SCALE as u32) * 2);
+            draw.text("BAAAM")
+                .x_y((N / 2) as f32 * SCALE, (N / 2) as f32 * SCALE)
+                .color(RED)
+                .font_size((SCALE as u32) * 2);
         }
     }
 }
 
-
-fn update(_app: &App, _m: &mut Model, _update: Update) {
-}
-
+fn update(_app: &App, _m: &mut Model, _update: Update) {}
 
 fn event(app: &App, m: &mut Model, event: WindowEvent) {
     let (x_temp, y_temp) = cord_2_field(app.mouse.position());
@@ -68,28 +79,29 @@ fn event(app: &App, m: &mut Model, event: WindowEvent) {
             if button == nannou::state::mouse::Button::Right {
                 m.playground.toggle_marker(x_temp as usize, y_temp as usize);
             }
-        },
+        }
         _other => (),
     }
 }
 
-
 fn model(app: &App) -> Model {
-    let _window = app.new_window().size(N as u32 * SCALE as u32, N as u32 * SCALE as u32)
-                                  .view(view)
-                                  .event(event)
-                                  .build()
-                                  .unwrap();
+    let _window = app
+        .new_window()
+        .size(N as u32 * SCALE as u32, N as u32 * SCALE as u32)
+        .view(view)
+        .event(event)
+        .build()
+        .unwrap();
     Model::new()
 }
 
-
 fn view(app: &App, m: &Model, frame: Frame) {
+    let translate = (-(N as f32) * 0.5 * SCALE) + SCALE / 2.0;
     // Begin drawing
-    let draw = app.draw();
+    let draw = app.draw().x_y(translate, translate);
     draw.background().color(LIGHTGRAY);
     // Draw field
     m.draw(&draw);
     // Write the result of our drawing to the window's frame.
-    draw.to_frame(app, &frame).unwrap();
+    draw.x_y(-100.0, -100.0).to_frame(app, &frame).unwrap();
 }
